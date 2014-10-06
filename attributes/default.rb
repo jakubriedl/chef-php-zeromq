bc9ab7cdbe7703942cbb34c30d7aa3079f48400f
Jakub Riedl <jakub.riedl@le.cz>
2014-10-06 13:46:06 +0200

    fix attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,12 +3,12 @@ default['php-zeromq']['url'] = 'https://github.com/mkoppanen/php-zmq/archive/mas
 case platform_family
 	when "rhel", "fedora"
 
-		default['php-msgpack']['conf_dirs'] = ['/etc/php.d']
-		default['php-msgpack']['conf_file'] = 'zeromq.ini'
+		default['php']['conf_dirs'] = ['/etc/php.d']
+		default['php-zeromq']['conf_file'] = 'zeromq.ini'
 
 	when "debian"
 
-		default['php-msgpack']['conf_dirs'] = ['/etc/php.d']
-		default['php-msgpack']['conf_file'] = '30-zeromq.ini'
+		default['php']['conf_dirs'] = ['/etc/php.d']
+		default['php-zeromq']['conf_file'] = '30-zeromq.ini'
 
 end
